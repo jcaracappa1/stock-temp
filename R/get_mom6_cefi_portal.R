a393b841e28b78dd7e48dbe07f3b689b69be5291
Joseph Caracappa (NOAA) <57966543+jcaracappa1@users.noreply.github.com>
2025-06-25 13:01:31 -0400

    add yellowtail

---
+++ R/get_mom6_cefi_portal.R
@@ -0,0 +1,10 @@
+#Script to get MOM6 data from CEFI portal
+
+hindcast.url.prefix = 'https://psl.noaa.gov/thredds/ncss/grid/Projects/CEFI/regional_mom6/cefi_portal/northwest_atlantic/full_domain/hindcast/monthly/regrid/r20230520/'
+hindcast.product.name = 'tob.nwa.full.hcast.monthly.regrid.r20230520.199301-201912.nc'
+lon.min = -78
+lon.max -62
+lat.min = 34
+lat.max = 47
+?var=tob&north=47&west=-78&east=-62&south=34&horizStride=1&time_start=1993-01-16T12:00:00Z&time_end=2019-12-16T12:00:00Z&&&accept=netcdf3
+
